5dc3f330380ba180f17248eb284851fce90252cf
Danny <dannydotdev@gmail.com>
2024-12-20 12:58:17 +0100

    Constant and Quadratic time complexity

--- Documents/Explanation.md
+++ Documents/Explanation.md
@@ -8,5 +8,15 @@ It has two important characteristics
 
 **Types of Big O**
 
-1. O(1) - Linear Time complexity :
+1. O(n) - Linear Time complexity :
    as the input size increases the time taken by the algorithm increases linearly.
+   _Most times if you see a loop in your algorithm, the time complexity is atleast linear_
+
+2. O(1) - Constant Time complexity :
+   as the input size increases the time taken by the algorithm remains constant.
+
+3. O(n^2) - Quadratic Time complexity :
+   as the input size increases the time taken by the algorithm increases quadratically.
+   _Most times if you see a nested loop in your algorithm, the time complexity is quadratic_
+
+- _The Big O drops the less dominant terms_
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Big O notation - Checking  time complexity of the function
+//Linear Time Complexity O(n)
 function summation(n) {
   let sum = 0; //executes once
   for (let i = 0; i <= n; i++) {
@@ -9,8 +9,23 @@ function summation(n) {
 }
 
 console.log(summation(4)); //Output: 10 (1 + 2 + 3 + 4)
-//Total count is n+2 given that n is the input of the function
-// and n =4 so total count is 4+2 =6
+//Total count is n+2 given that n is the input of the function and n =4 so total count is 4+2 =6
 //So the time complexity is dependent on the input size Big-O is expressed in terms of the input size
 
 //Big O focuses on the bigger picture, as the input size increases, the 2 stays constant until it becomes insignificant because contributes to the total value
+
+//Constant Time Complexity   O(1)
+function addition(n) {
+  return (n * (n + 1)) / 2; //irrespective of the input size, the line will always execute once
+}
+console.log(addition(5)); //Output: 15
+
+//Quadratic Time Complexity O(n^2)
+function quadratic(n) {
+  for (let i = 0; i <= n; i++) {
+    for (let j = 0; j <= n; j++) {
+      console.log(i, j);
+    }
+  }
+}
+quadratic(3);
